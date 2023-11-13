66fac53fd4bf0eaafe4407516853747cd53d99f5
dongmin-Lim <ldm96216@gmail.com>
2023-11-13 20:54:55 +0900

    드롭다운 로컬스토리지 보관

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import Main from "./views/mainPage/Main";
 import ListMain from "./views/newsListPage/ListMain";
 import NavBar from "./components/ui/NavBar";
 import GlobalStyle from "./styles/GlobalStyle"; // 글로벌 스타일 파일을 가져옵니다.
-import typographyStyles from "./styles/typography";
 
 function App() {
+  if (window.localStorage.getItem("type") === null) {
+    window.localStorage.setItem("type", "title,content");
+  }
   return (
     <div>
       <GlobalStyle />
-      <typographyStyles>
-        <NavBar />
-        <div style={{ paddingTop: "5rem", display: "flex" }}>
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path="/list" element={<ListMain />} />
-          </Routes>
-        </div>
-      </typographyStyles>
+      <NavBar />
+      <div style={{ paddingTop: "5rem", display: "flex" }}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/list" element={<ListMain />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/components/ui/Dropdown.jsx
+++ src/components/ui/Dropdown.jsx
@@ -1,20 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 
 export default function Dropdown() {
   const [selectedItem, setSelectedItem] = useState("제목/내용");
 
   const handleDropdownChange = (event) => {
+    window.localStorage.setItem("type", event.target.value);
     setSelectedItem(event.target.value);
   };
+
+  useEffect(() => {
+    // 로컬 스토리지에서 저장된 배열을 가져옵니다.
+    const storedSort = localStorage.getItem("type");
+    if (storedSort) {
+      setSelectedItem(storedSort);
+    }
+  }, []);
 
   return (
     <div>
       <FormControl>
         <Select value={selectedItem} onChange={handleDropdownChange}>
-          <MenuItem value="제목/내용">제목/내용</MenuItem>
-          <MenuItem value="제목">제목</MenuItem>
-          <MenuItem value="내용">내용</MenuItem>
+          <MenuItem value="title,content">제목/내용</MenuItem>
+          <MenuItem value="title">제목</MenuItem>
+          <MenuItem value="content">내용</MenuItem>
         </Select>
       </FormControl>
     </div>
