da0097a9fb9ceabd912ad34d394f39f27b5080e7
Jin-Mook <jink0608@naver.com>
2024-03-06 16:07:56 +0900

    [LF-51/cicd] .env 설정 가져오는 변수 수정

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
 // Axios 전역 설정
-axios.defaults.baseURL = process.env.REACT_APP_DEFAULTS_BASEURL;
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_IP;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
